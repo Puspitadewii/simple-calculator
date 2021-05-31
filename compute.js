a843b6583bdafa200258bed6847dc06e3b69ce47
Puspitadewii <dewipuspitt4@gmail.com>
2021-05-31 10:20:55 +0700

    modified file

--- compute.js
+++ compute.js
@@ -1,7 +1,15 @@
 function forclear(){
     document.getElementById("result").innerHTML = "0";
 }
+function removeZero(){
+    let value = document.getElementById("result").innerHTML;
+    if(value == "0"){
+        value = "";
+        document.getElementById("result").innerHTML = value;
+    }
+}
 function perc(){
+    removeZero();
     var value = document.getElementById("result").innerHTML;
     value = value / 100;
     document.getElementById("result").innerHTML = value;
@@ -10,20 +18,19 @@ function fordisplay(display){
     removeZero();
     document.getElementById("result").innerHTML += display;
 }
-function removeZero(){
-    let value = document.getElementById("result").innerHTML;
-    if(value == "0"){
-        value = "";
-        document.getElementById("result").innerHTML = value;
-    }
-}
+
 
 
 function solve(){
     removeZero();
-    var equation = document.querySelector("#result").innerHTML;
+    var equation = document.getElementById("result").innerHTML;
     let solved = eval(equation);
-    document.querySelector("#result").innerHTML;
+    document.getElementById("result").innerHTML = solved;
 }
+const output = document.getElementById("result").value;
+console.log(output);
+
+
+
 
 
